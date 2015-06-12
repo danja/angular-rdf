81a6e61d58ad6677d3bd0116f4959540992812f6
Danny Ayers <danny.ayers@gmail.com>
2015-06-12 23:09:03 +0200

    tweak

--- utils/server.js
+++ utils/server.js
@@ -2,4 +2,4 @@
 
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(../public)).listen(8088);
+connect().use(serveStatic("../public")).listen(8088);
