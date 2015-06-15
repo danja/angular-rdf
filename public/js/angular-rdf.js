e0ee74cd8701d33ca304af5ccb26a3a8f55cd260
Danny Ayers <danny.ayers@gmail.com>
2015-06-15 20:40:00 +0200

    nearly there...

--- public/js/angular-rdf.js
+++ public/js/angular-rdf.js
@@ -7,9 +7,20 @@ var subject = "http://localhost:8080/data/person/sheldon-cooper";
 
 var app = angular.module('AngularRDF', []);
 
+app.directive("myDirective", function () {
+    return {
+        restrict: "A",
+        scope: {
+            two: "="
+        }
+    };
+});
+
 // controller here
-app.controller('rdfController', ['$scope', '$q', '$log',
-    function ($scope, $q, $log) {
+app.controller('rdfController', ['$scope', '$attrs', '$q', '$log',
+    function ($scope, $attrs, $q, $log) {
+
+        $log.info("PROPERTY = " + $attrs.property);
 
         //  .controller('LogController', ['$scope', '$log', function($scope, $log) {
         $scope.$log = $log;
@@ -18,90 +29,11 @@ app.controller('rdfController', ['$scope', '$q', '$log',
 
         $scope.store = new rdf.LdpStore();
 
-        // start node
-        //   $scope.rdfNode = $scope.cf.node($scope.subject);
-
-        // $scope.node = new Object();
-        // $scope.givenName = "";
-        //   $scope.familyName = "";
-
         $scope.data = {
             message: "Helloo"
         };
 
         // Angular has its own $http, see http://stackoverflow.com/questions/21667613/in-angular-how-to-pass-json-object-array-into-directive
-        /*
-            $scope.store.graph(dataUrl, function (graph) {
-                $log.info("graph = \n" + graph);
-                //   console.log(graph.toArray()[0].object.toString());
-                $scope.turtleString = graph.toArray().join("\n").toString();
-                $scope.$apply(); // trigger sync
-                //   $log.info("loading cf...");
-                // $scope.cf = rdf.cf.Store($scope.store); // clownface, for graph traversal
-
-                //   $log.info("cf loaded" + cf);
-                // console.log($scope.turtleString);
-            });
-            */
-
-        $scope.store.graph(dataUrl, function (graph, error) {
-            $scope.graph = graph;
-            if (error) {
-                console.log(error);
-            } else {
-                // note that graph.length is a value, not a function
-                console.log("Successfully fetched %d triples", graph.length);
-                $scope.turtleString = graph.toArray().join("\n").toString();
-                         $scope.$apply(); // trigger sync
-              //  $scope.$apply(); // trigger sync
-                var knows = graph.match(null, 'http://schema.org/givenName', null);
-
-                knows.forEach(function (triple) {
-                    // iterate over the results, creating a filtered graph for each subject found
-                    // and pass that graph to a display function
-
-                    // complete triple in N-Triples notation
-                    console.log("Triple:    " + triple.toString());
-
-                    // and subject - predicate - object as strings
-                    console.log("Subject:   " + triple.subject);
-                    console.log("Predicate: " + triple.predicate);
-                    console.log("Object:    " + triple.object);
-                    console.log("---------------------------------------------------------------------");
-                });
-
-
-                // this one
-                $scope.node = {};
-
-
-                $scope.node.object = function (name, value) {
-
-                    $log.info("paramsname = " + name + " value = " + value);
-
-                    var split = name.split(":");
-                    if (split.length > 1) {
-                        name = namespaceMap[split[0]] + split[1];
-                    }
-                    console.log("name = " + name);
-                    $log.info("name = " + name);
-                    // $log.info("cf = " + $scope.cf);
-
-                    if (angular.isDefined(value) && value != null) {
-
-                        setLiteral(subject, name, value);
-                        // return "r";
-                    } else {
-                        var objectNode = getLiteral(subject, name);
-                        console.log("returning name = " + objectNode);
-
-               
-                        return objectNode;
-                    }
-                }
-
-            }
-        });
 
         /**
          * Represents a book.
@@ -110,27 +42,81 @@ app.controller('rdfController', ['$scope', '$q', '$log',
          * @param {string} author - The author of the book.
          * @returns {Number}
          */
+        $scope.store.graph(dataUrl, function (graph, error) {
+            $scope.graph = graph;
+            if (error) {
+                console.log(error);
+            } else {
+               
+                console.log("Successfully fetched %d triples", graph.length);
+                
+                 // render data
+                $scope.turtleString = graph.toArray().join("\n").toString();
+                $scope.$apply(); // trigger sync
+
+                $scope.node = {};
+                
+                $scope.node.x = function (value) {
+                    
+                    var name = $attrs.property;
+                    $log.info("value  in node.x = " + value);
+
+                    var split = name.split(":");
+                    if (split.length > 1) {
+                        name = namespaceMap[split[0]] + split[1];
+                    }
+
+                    $log.info("name = " + name);
+                    // $log.info("cf = " + $scope.cf);
+                    $log.info("calling setLiteral with " + subject + "  " + name + "  " + value);
+                    if (value) { // angular.isDefined(value) && 
+
+                        setLiteral(subject, name, value);
+                        // return "r";
+                    } else {
+                        var objectNode = getLiteral(subject, name);
+                        console.log("returning name = " + objectNode);
+
+
+                        return objectNode;
+                    }
+                }
+
+                $scope.node.object = function (name, value) {
+                    var name = $attrs.property;
+                    $log.info("value in node.object = " + value+ " name = "+name);
+
+                    var split = name.split(":");
+                    if (split.length > 1) {
+                        name = namespaceMap[split[0]] + split[1];
+                    }
+
+                    $log.info("name = " + name);
+                    // $log.info("cf = " + $scope.cf);
+                    $log.info("calling setLiteral with " + subject + "  " + name + "  " + value);
+                    if (value && (typeof(value) != "undefined") && (""+value) != "undefined") { // angular.isDefined(value) && 
+
+                        setLiteral(subject, name, value);
+                        // return "r";
+                    } else {
+                        var objectNode = getLiteral(subject, name);
+                        console.log("returning name = " + objectNode);
+                        return objectNode;
+                    }
+                }
+
+            }
+        });
+
+
         function getLiteral(s, p) {
             $log.info("getLiteral called with s=" + s + " p=" + p);
 
             var matches = $scope.graph.match(s, p, null);
-//            console.log("matches = " + matches.length);
-
-            /*
-            matches.forEach(function (triple) {
-                // iterate over the results, creating a filtered graph for each subject found
-                // and pass that graph to a display function
-
-                // complete triple in N-Triples notation
-                console.log("Triple:    " + triple.toString());
-
-                // and subject - predicate - object as strings
-                console.log("Object:   " + triple.subject);
-            }
-                            */
+            //            console.log("matches = " + matches.length);
             matches = matches.toArray();
             var o = matches[0].object.toString();
-            console.log("o = "+o);
+            console.log("getLiteral o = " + o);
             return o;
 
             // clownface lookup
@@ -145,75 +131,14 @@ app.controller('rdfController', ['$scope', '$q', '$log',
         }
 
         // test it clears multiple values in graph
-        function setLiteral(s, p, o) {
-            $log.info("setLiteral called with s=" + s + " p=" + p + " o=" + o);
-            // check o is a string
-        //    $scope.graph.removeMatches(s, p, null);
+        function setLiteral(sString, pString, oString) {
+            $log.info("setLiteral called with s=" + sString + " p=" + pString + " o=" + oString);
+            // todo : check o is a string
+            var s = rdf.createNamedNode(sString)
+            var p = rdf.createNamedNode(pString);
+            var o = rdf.createLiteral(oString, null, null);
+
+            $scope.graph.removeMatches(s, p, null);
             $scope.store.add(s, p, o);
         }
-
-        /*
-            //  $log.info("setLiteral $scope.cf=" + $scope.cf);
-            if (getLiteral(s, p)) {
-                $scope.cf.node(s).removeOut(p);
-            }
-
-            // add new triple
-            $scope.cf.node(o, null, function (result) {
-                // anything?
-            })
-                .catch(function (error) {
-                    $log.info("error: " + error);
-                });
-                */
-
-                }]);
-
-
-
-
-
-
-
-
-
-/*
-angular.module('controllerAsExample', [])
-  .controller('SettingsController1', SettingsController1);
-
-function SettingsController1() {
-  this.name = "John Smith";
-  this.contacts = [
-    {type: 'phone', value: '408 555 1212'},
-    {type: 'email', value: 'john.smith@example.org'} ];
-}
-
-
-var app = angular.module('myApp', []);
-    var store = new rdf.LdpStore();
-    store.match("http://tbbt.zazukoians.org/data/person/sheldon-cooper",
-                    null,
-                    null,
-                    null,
-                    function (data) {
-    console.log(data.toString());
-    app.controller('personCtrl', RdfNode(data, "http://tbbt.zazukoians.org/data/person/sheldon-cooper"));
-});
-
-
-// in this example we create an LdpStore as this one is able to fetch external URIs
-var store = new rdf.LdpStore();
-
-// we use the store.graph() function to resolve an external URI
-// the callback returns a RDF Interface graph object
-store.graph('http://www.w3.org/People/Berners-Lee/card', function(graph, error){
-
-  if(error)
-  {
-    console.log(error);
-  } else {
-    // note that graph.length is a property, not a function
-    console.log("Successfully fetched %d triples", graph.length);
-  }
-});
-*/
+}]);
